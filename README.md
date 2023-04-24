09b5ae35254afd79cc9027bcd398ab55190a1ee3
pottsaf <aaronfpotts@gmail.com>
2023-04-23 18:52:35 -0600

    Add password criteria and matching with prompts

--- README.md
+++ README.md
@@ -1,5 +1,19 @@
 # signup_form
 
-- Make form
-- Clean up appearance
-- Make three layouts depending on viewport (Large, medium, small or laptop, pad, phone)
+- [x] Make form
+- [x] Clean up appearance
+- [x] Make three layouts depending on the viewport (Large, medium, small or laptop, pad, phone)
+    - Made this a responsive page for across-the-board as opposed to three layouts.
+- [ ] Password 
+    - [ ] Show passwords
+    - [ ] Show criteria
+    - [ ] Confirmation
+    - [ ] Submission with console.table
+- [ ] Success modal
+    - [ ] Confirmation modal before changing to actually submitting
+    - [ ] Redirect page from signup form upon successful submission
+- [ ] Settings (CRUD)
+    - [ ] Be able to change:
+        - [ ] Information
+        - [ ] Password
+    - [ ] Deletion of user
---
+++ scripts.js
@@ -0,0 +1,49 @@
+console.log('Running script')
+
+const input = document.querySelectorAll("input");
+
+input.forEach(function(input) {
+  input.addEventListener("focus", function() {
+    console.log('Input has focus');
+  });
+  input.addEventListener("blur", () => {
+    console.log('Bye');
+  });
+});
+
+const passwordInput = document.getElementById("password1");
+const passwordCriteria = document.getElementById("password-criteria");
+const passwordInput2 = document.getElementById("password2");
+const message = document.querySelector("p");
+
+const criteriaRegex = [
+  /^.{8,20}$/,
+  /[A-Z]/,
+  /[a-z]/,
+  /\d/
+];
+
+passwordInput.addEventListener("input", function() {
+  const password = passwordInput.value;
+  for (let i = 0; i < criteriaRegex.length; i++) {
+    const regex = criteriaRegex[i];
+    const criteriaItem = passwordCriteria.children[i];
+    if (regex.test(password)) {
+      criteriaItem.classList.remove("invalid");
+      criteriaItem.classList.add("valid");
+    } else {
+      criteriaItem.classList.remove("valid");
+      criteriaItem.classList.add("invalid");
+    }
+  }
+});
+
+passwordInput2.addEventListener("input", function() {
+  if (passwordInput.value === passwordInput2.value) {
+    message.classList.remove("invalid");
+    message.classList.add("valid");
+  } else {
+    message.classList.remove("valid");
+    message.classList.add("invalid");
+  }
+});
