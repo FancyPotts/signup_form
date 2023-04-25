0408d9dcb1054dc92a91782055c403aea6e05895
pottsaf <aaronfpotts@gmail.com>
2023-04-24 18:17:50 -0600

    Add psuedo database, function for new users, add users to database

--- scripts.js
+++ scripts.js
@@ -1,12 +1,10 @@
-console.log('Running script')
-
-
 const passwordInput = document.getElementById("password1");
 const passwordCriteria = document.getElementById("password-criteria");
 const passwordInput2 = document.getElementById("password2");
 const message = document.querySelector("p");
 const button = document.querySelector("button");
 const input = document.querySelectorAll("input");
+const database = [];
 
 const criteriaRegex = [
   /^.{8,20}$/,
@@ -40,11 +38,26 @@ passwordInput2.addEventListener("input", function() {
   }
 });
 
+function UserSignUp(postFirstName, postLastName, postEmail, postPhoneNum) {
+  this.firstName = postFirstName,
+  this.lastName = postLastName,
+  this.email = postEmail,
+  this.phoneNum = postPhoneNum
+};
+
 button.addEventListener("click", () => {
   const firstNameInput = document.querySelector('#firstname');
   const lastNameInput = document.querySelector('#lastname');
+  const emailInput = document.querySelector('#email');
+  const phoneNumInput = document.querySelector('#phonenum');
 
-  
+  postFirstName = firstNameInput.value
+  postLastName = lastNameInput.value
+  postEmail = emailInput.value
+  postPhoneNum = phoneNumInput.value
+
+  const User = new UserSignUp(postFirstName, postLastName, postEmail, postPhoneNum);
+  database.push(User);
 });
 
 input.forEach(function(input) {
